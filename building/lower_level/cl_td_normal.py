d2c107e87b5289cbc469195c7d94c3797494656c
Parvfect <parvagrw02@gmail.com>
2023-04-24 17:12:45 +0100

    ehre we go

--- building/lower_level/cl_td_normal.py
+++ building/lower_level/cl_td_normal.py
@@ -5,16 +5,16 @@ import numpy as np
 from helper_functions import *
 
 
-n = 15 # Hilbert Space Dimension
-gamma = 0.001 # Damping Rate
+n = 14 # Hilbert Space Dimension
+gamma = 0.05 # Damping Rate
 
 # Annihilation and Creation Operators
-Ldag = create_annihilation_operator(n)
+adag = create_annihilation_operator(n)
 a = create_creation_operator(n)
 
 # Hamiltonian
-q = (Ldag + a)/2
-p = 1j*(Ldag - a)/2
+q = (adag + a)/2
+p = 1j*(adag - a)/2
 H = (np.dot(p,p) + np.dot(q,q)) + gamma/2 * get_anti_commutator(q,p)
 H = np.array(H)
 
@@ -29,7 +29,7 @@ T = 300
 nth = 1/(np.exp((hbar*w)/(kb*T))-1) #2
 sz = np.array([[1,0],[0,-1]])
 wo = 10
-L = q + p
+L = q + (1j)*p
 Ldag = np.conjugate(L).T
 #H = (wo/2) * sz 
 
@@ -40,16 +40,16 @@ def LinEm(x):
     hamiltonian_part = (-1j)* (np.dot(H, x) - np.dot(x, H))
     lindblad_part_1 = get_commutator(L, np.dot(x, Ldag))
     lindblad_part_2 = get_commutator(np.dot(L, x), Ldag)
-    lindblad_part_3 = get_commutator(Ldag, np.dot(x, L))
-    lindblad_part_4 = get_commutator(np.dot(Ldag, x), L)
+    #lindblad_part_3 = get_commutator(Ldag, np.dot(x, L))
+    #lindblad_part_4 = get_commutator(np.dot(Ldag, x), L)
     
-    return hamiltonian_part + 0.5*(lindblad_part_1 + lindblad_part_2 + lindblad_part_3 + lindblad_part_4)    
+    return hamiltonian_part + 0.5*gamma*(lindblad_part_1 + lindblad_part_2)    
 
 if __name__ == "__main__":
     init = make_initial_density_matrix(n)
     t_i = 0
-    t_f = 100
-    nsteps = 800
+    t_f = 1000
+    nsteps = 10000
 
     h = (t_f-t_i)/nsteps
     solRK = np.zeros((nsteps+1,n, n),dtype=complex)
@@ -60,6 +60,6 @@ if __name__ == "__main__":
 
     # Visualising
     plot_density_matrix_elements(solRK, title="QHO Thermal Bath with {}states".format(n))
-    plot_trace_purity(solRK, title="QHO Thermal Bath with {}states".format(n))
+    #plot_trace_purity(solRK, title="QHO Thermal Bath with {}states".format(n))
 
     plot_steady_state_td(solRK, title="Calderia Leggett with {}states".format(n))
